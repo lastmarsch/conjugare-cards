88e4209223d18b0f0ed6c0f54d21edd981781d08
Violet <eerfeer@yandex.ru>
2022-11-28 16:51:04 +0300

    Add conjugare parser

--- packages/server/controllers/conjugare.ts
+++ packages/server/controllers/conjugare.ts
@@ -1,12 +1,29 @@
+import axios from 'axios';
 import express from 'express';
+import { conjugareTransformer } from '../helpers/conjugareTransformer';
+
+const DEXONLINE_DEFINITIE_URL = `https://dexonline.ro/definitie`;
 
 const conjugareRouter = express.Router({
   mergeParams: true,
 });
 
 conjugareRouter.get('/:verb', (req, res) => {
-  console.log(req.params);
-  res.json('It works!');
+  const { verb } = req.params;
+
+  axios
+    .get(`${DEXONLINE_DEFINITIE_URL}/${verb}/paradigma`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+    .then((response) => {
+      const { data } = response;
+      const conjugare = conjugareTransformer(data);
+      res.json(conjugare);
+    })
+    .catch((e) => res.status(e.status).send(e.message));
 });
 
 export { conjugareRouter };
